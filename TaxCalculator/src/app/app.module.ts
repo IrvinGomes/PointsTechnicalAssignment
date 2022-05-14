5b29413cacc3635d3671f71b59a1f3ca90f88020
IrvinGomes <irvin.r.gomes@outlook.com>
2022-05-14 16:06:25 -0300

    calculating with with default api call

--- TaxCalculator/src/app/app.module.ts
+++ TaxCalculator/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+/**Angular Material */
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { CalculationComponent } from './components/calculation/calculation.component';
 
 @NgModule({
@@ -13,8 +19,15 @@ import { CalculationComponent } from './components/calculation/calculation.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    /**Angular Material */
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- TaxCalculator/src/app/services/brackets/brackets.service.ts
+++ TaxCalculator/src/app/services/brackets/brackets.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Brackets } from 'src/app/model/bracket';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BracketsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  url = environment.api.url + 'brackets';
+
+  get(): Observable<Brackets> {
+    return this.http.get<Brackets>(this.url);
+  }
+
+  getByYear(year: string): Observable<Brackets> {
+    return this.http.get<Brackets>(this.url + '/' + year);
+  }
 }
